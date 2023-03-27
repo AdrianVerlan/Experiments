be5946d461126b2c07f8839d8fa0493263677ef5
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-03-27 18:46:13 +0300

    Add files via upload

--- alarm_app.py
+++ alarm_app.py
@@ -1,5 +1,6 @@
 import pygame
 import datetime
+from os import system
 
 alarm_h = int(input("Write alarm hour: "))
 alarm_m = int(input("Write alarm minutes: "))
@@ -8,7 +9,7 @@ pygame.mixer.init()
 pygame.mixer.music.load("testare_alarma.mp3")
 
 while True:
-
+    system("cls")
     current_time = datetime.datetime.now()
     current_hour = current_time.hour
     current_minute = current_time.minute
@@ -16,10 +17,8 @@ while True:
     seconds_left = 60 - current_time.second
     if seconds_left == 60:
         seconds_left= 0
-        minutes_left +=1
-    if minutes_left <0:
-        minutes_left += 60
-    print(f"Time to alarm : {minutes_left:02d}:{seconds_left:02d}")
+       
+    print(f"Time to alarm :{seconds_left:02d}")
 
     if current_hour== alarm_h and current_minute == alarm_m:
         print()
